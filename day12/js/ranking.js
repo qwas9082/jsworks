26d45589ddb3c8c186118f4bdb9a898f4dcd6561
qwas9082 <qwas9082@gmail.com>
2022-05-24 15:41:54 +0900

    day12

---
+++ day12/js/ranking.js
@@ -0,0 +1,15 @@
+let ranking = 1;
+        switch(ranking){
+            case 1:   //ranking =1
+                document.write("금메달입니다<br>");
+                break;
+            case 2:
+                document.write("은메달입니다<br>");
+                break;
+            case 3:
+                document.write("동메달입니다<br>");
+                break;
+            default:
+                document.write("메달이없습니다.<br>");
+                break;
+        }
