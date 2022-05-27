1fb56c33b5598e94e494628ea162b91c735e70cd
qwas9082 <qwas9082@gmail.com>
2022-05-27 18:18:09 +0900

    day 15

---
+++ day15/js/flower.js
@@ -0,0 +1,20 @@
+function showText(){
+    //텍스트 보이기 
+    let text = document.getElementById('detail');
+    text.style.display = "block"
+    // document.getElementById('detail').style.display= "block";
+
+    //보기 버튼은 숨기기
+    let button = document.getElementById('show');
+    button.style.display = "none"
+}
+
+function hideText(){
+    //텍스트는 숨기기
+    let text = document.getElementById('detail');
+    text.style.display = "none"
+
+    //보기 버튼 보이기
+    let button = document.getElementById('show');
+    button.style.display = "block"
+}
