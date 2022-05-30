686fee164e0888142182a4ad0a021efbdd9a16be
qwas9082 <qwas9082@gmail.com>
2022-05-30 18:19:27 +0900

    day16

---
+++ day16/js/alphabet.js
@@ -0,0 +1,20 @@
+//대문자 26자를 배열에 저장
+//배열에 26개의 공간을 생성
+let alphabet = new Array(26);
+let ch = 'A'; 
+ch = ch.charCodeAt(0);  //코드값 65
+
+// console.log(ch);
+// console.log(ch.charCodeAt(0)); //65
+
+for(var i = 0; i < alphabet.length; i++){
+    alphabet[i] = ch; //0번 인덱스에 65가 저장됨
+    ch++;     //코드(숫자) 1증가
+}
+//65, 66, 67 ..... 90
+
+//출력
+for(var i = 0; i < alphabet.length; i++){
+    console.log(alphabet[i] + "," + 
+        String.fromCharCode(alphabet[i]))
+}
